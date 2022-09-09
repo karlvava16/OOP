6de89dc1c0263dd9637232319565016d4ab4bf85
karlvava16 <91985923+karlvava16@users.noreply.github.com>
2022-09-09 13:43:48 +0300

    merge errors fixed

--- MyString07.09.2022/MyString07.09.2022/MyString.h
+++ MyString07.09.2022/MyString07.09.2022/MyString.h
@@ -7,10 +7,7 @@ class MyString
 private:
 	char* str; // pointer to string
 	int length; // string length
-<<<<<<< HEAD
 	static int count; // number of objects
-=======
->>>>>>> 2b3e558cecdec2472a1c854aac710f37175a2567
 
 public:
 	MyString(); // default constructor
@@ -29,8 +26,5 @@ public:
 	void MyDelChr(char c); // delete character from string 
 	int MyStrCmp(MyString& b); // compare strings
 	const char* MyC_Str(); // get const pointer to char array
-<<<<<<< HEAD
 	static int GetCount(); // get number of objects
-=======
->>>>>>> 2b3e558cecdec2472a1c854aac710f37175a2567
 };
