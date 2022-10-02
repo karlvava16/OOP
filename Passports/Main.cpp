cfaa80a0889c467e9d326da3d40054b6ccc715f6
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2022-10-02 16:47:23 +0300

    inheritance 30.09.2022

--- Passports/Main.cpp
+++ Passports/Main.cpp
@@ -22,3 +22,37 @@ int main()
 
 	return 0;
 }
+
+/*RESULT*/
+
+/*
+Vladyslav Karlinskyi M Odessa/UKR UKR 20040123-12345 012345678 0123 23 01 2004 23 01 2032 23 01 2022
+
+Name : Vladyslav
+Surname : Karlinskyi
+Sex : M
+Place of birth : Odessa/UKR
+Nationality : UKR
+RecordNo. : 20040123-12345
+DocumentNo. : 012345678
+Authority : 0123
+Date of birth : 23.01.2004
+Date of exiry : 23.01.2032
+Date of issue : 23.01.2022
+
+
+Type : T
+Country code : UKR
+Name : Vladyslav
+Surname : Karlinskyi
+Sex : M
+Place of birth : Odessa/UKR
+Nationality : UKR
+RecordNo. : 20040123-12345
+DocumentNo. : 876543210
+Authority : 3201
+Date of birth : 23.01.2004
+Date of exiry : 23.01.2032
+Date of issue : 23.01.2022
+
+*/
